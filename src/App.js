79e00dc7dac5115447f721645ef743172f5d034c
Evangelos Giannakosian <vgiannakosian@pavla.gr>
2021-01-13 20:10:46 +0200

    created SearchBox added cards filtering using the search input

---
+++ src/App.js
@@ -0,0 +1,40 @@
+import React from "react";
+import {robots} from "./robots";
+import CardList from "./CardList/CardList";
+import SearchBox from "./SearchBox/SearchBox";
+
+class App extends React.Component {
+
+    constructor() {
+        super();
+        this.state = {
+            robots: robots,
+            searchField: ''
+        }
+    }
+
+    onSearchChange = ({target: {value}}) => {
+        this.setState({
+            searchField: value
+        });
+    }
+
+    render() {
+
+        const filteredRobots = this.state.robots.filter(robot => {
+            return robot.name.toLowerCase().includes(this.state.searchField.toLowerCase())
+        })
+
+        return (
+            <div className="tc">
+                <h1>RoboFriends</h1>
+                <SearchBox searchChange={this.onSearchChange}/>
+                <CardList robots={filteredRobots}/>
+            </div>
+        );
+    }
+
+}
+
+
+export default App
