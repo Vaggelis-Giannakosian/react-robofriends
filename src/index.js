9953db8a4d43f9947dd43f23f9624b39519a6608
Evangelos Giannakosian <vgiannakosian@pavla.gr>
2021-01-21 11:56:33 +0200

    added icons

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ import './index.css';
 import reportWebVitals from './reportWebVitals';
 import App from './containers/App/App'
 import 'tachyons';
-import * as serviceWorker from "./serviceWorkerRegistration";
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 
 
 const logger = createLogger();
@@ -38,10 +38,13 @@ ReactDOM.render(
     document.getElementById('root')
 );
 
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://cra.link/PWA
+serviceWorkerRegistration.register();
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
-
-serviceWorker.register();
